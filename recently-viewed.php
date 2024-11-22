960f9ef51631d047817876be4d0b9a81981d9ba6
Bobby McNCheese <chonbadincheingthong@gmail.com>
2024-11-22 23:16:45 +0700

    make ajax-update-cart.php and recently-viewed.php

---
+++ recently-viewed.php
@@ -0,0 +1,31 @@
+<?php
+
+@session_start();
+
+if (isset($_SESSION['recently_viewed'])) {
+
+    echo <<<HTML
+    <hr>
+    <div class="my-4 text-center">
+        <h6 class="text-secondary">Recently Viewed Products</h6>
+        <div class="d-flex mt-3 justify-content-center">
+    HTML;
+
+    $i = 1;
+    foreach ($_SESSION['recently_viewed'] as $rv) {
+
+        echo <<<HTML
+        <div class="d-flex flex-column justify-content-between border p-2 mr-2 text-center" style="max-width:100px;">
+            $rv
+        </div>
+        HTML;
+
+        if ($i == 20) { break; }
+        $i++;
+    }
+
+    echo '<div>
+    </div>';
+}
+
+?>
